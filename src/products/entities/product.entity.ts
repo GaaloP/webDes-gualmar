3a32b41085ea4e20c01222e3136d6a399265d051
GaaelP <gposada24@alumnos.uaq.mx>
2024-09-14 16:03:41 -0600

    Docker and Database03

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,16 @@
-export class Product {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { UUID } from "typeorm/driver/mongodb/bson.typings";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn("uuid")
+    productId: string;
+    @Column({type: "text"})
+    productName: string;
+    @Column({type: "float"})
+    price: number;
+    @Column({type: "int"})
+    couneSeal: number;
+    @Column({type: "uuid"})
+    provider: string;
+}
