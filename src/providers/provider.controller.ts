8ba87059e7172a5693a7f5fdee7063e819c1a992
GaaelP <gposada24@alumnos.uaq.mx>
2024-10-05 17:30:38 -0600

    Auth for entitys

--- src/providers/provider.controller.ts
+++ src/providers/provider.controller.ts
@@ -5,28 +5,32 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constant';
 
 @Controller('providers')
 export class ProviderController {
   constructor(private readonly providerService: ProviderService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providerService.create(createProviderDto);
   }
 
-  @Auth("Employee")
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee")) throw new UnauthorizedException("no se pode")
     return this.providerService.findAll();
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get('/name/:name')
   findByName(@Param('name') name: string) {
     return this.providerService.findByName(name)
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Get(':id')
   findOne(@Param('id') id: string) {
     const provider = this.providerService.findOne(id);
@@ -34,11 +38,13 @@ export class ProviderController {
     return provider
   }
 
+  @Auth(ROLES.MANAGER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
     return this.providerService.update(id, updateProviderDto);
   }
-
+  
+  @Auth(ROLES.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.providerService.remove(id);
